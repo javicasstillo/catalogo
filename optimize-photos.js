aec5bc35cbd167e3035fa3cad2f8cc75093caa70
1812 <francogadea1812@gmail.com>
2026-01-02 16:06:55 -0300

    feat: initialize project with package.json and package-lock.json
    - Added package.json to define project metadata and dependencies.
    - Included package-lock.json to lock dependency versions for consistent installs.
    - Set up sharp as a dependency for image processing.

---
+++ optimize-photos.js
@@ -0,0 +1,52 @@
+import path from "node:path";
+import fs from "node:fs";
+import sharp from "sharp";
+
+
+const TARGET = path.resolve(import.meta.dirname, './assets/Fotos/Fotos página web');
+const OUTPUT = path.resolve(import.meta.dirname, './assets/Fotos/ultra-optimized');
+
+
+async function optimizeImages(opt = {}) {
+    
+    const {folder, output, recursive, ext = ['.jpg', '.jpeg', '.png']} = opt;
+
+    const files = await fs.promises.readdir(folder);
+
+    for (const file of files) {
+
+        const filePath = path.resolve(folder, file);
+
+        const isDirectory = (await fs.promises.stat(filePath)).isDirectory();
+
+        if(isDirectory && recursive){
+
+            await optimizeImages({...opt, folder: filePath, output: path.join(output, file)});
+        }
+        else {
+
+            const extname = path.extname(filePath);
+            const filename = path.basename(filePath, extname);
+            const outfilepath = path.join(output, `${filename}.webp`);
+
+            if(ext.includes(extname)){
+
+                console.log('Process:', filePath);
+
+                if(!fs.existsSync(output)){
+
+                    await fs.promises.mkdir(output, {recursive: true});
+                }
+                
+                await sharp(filePath, {})
+                    .webp({quality: 50})
+                    .toFile(outfilepath);
+
+                console.log('Complete:', outfilepath);
+            }
+        }
+    }
+}
+
+
+await optimizeImages({folder: TARGET, output: OUTPUT, recursive: true});
